45e6216be438cb10d4170f7dd84ae99c85385916
yahya-sattar <ysatt001@ucr.edu>
2025-11-30 15:38:11 +0000

    Deploying to gh-pages from @ yahya-sattar/yahya-sattar.github.io@7a82c176e9e32f2fb02f5c0404b5f39c8ffeed57 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -344,7 +344,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/assets/pdf/CV_YS.pdf";
           
         },
       },{id: "post-a-post-with-diagrams",
@@ -505,21 +505,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_YS.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%79%73%61%74%74%61%72@%63%6F%72%6E%65%6C%6C.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -533,14 +526,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=fqgk10cAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
