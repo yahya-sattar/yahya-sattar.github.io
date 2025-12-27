ae1a31c2122e09dba76227bab528c4656231bd66
yahya-sattar <ysatt001@ucr.edu>
2025-12-27 04:42:59 +0000

    Deploying to gh-pages from @ yahya-sattar/yahya-sattar.github.io@e93474ac5f820e3e5672192fb975d5e0c96f9a7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,12 +419,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-paper-published-in-neurips",
-          title: 'Paper published in NeurIPS',
+            },},{id: "news-our-paper-titled-pre-trained-large-language-models-learn-hidden-markov-models-in-context-got-published-in-neurips-details-title-pre-trained-large-language-models-learn-hidden-markov-models-in-context-neurips-2025-y-dai-z-gao-y-sattar-s-dean-j-j-sun",
+          title: 'Our Paper titled “Pre-trained Large Language Models Learn Hidden Markov Models In-context” got...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_1/";
-            },},{id: "news-paper-published-in-neurips",
+          section: "News",},{id: "news-paper-published-in-neurips",
           title: 'Paper published in NeurIPS',
           description: "",
           section: "News",handler: () => {
